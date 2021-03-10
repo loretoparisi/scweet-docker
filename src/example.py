e2be42eff2a0c1ce665d2fcab07ce86e22a3ec52
Loreto Parisi <loretoparisi@gmail.com>
2021-03-10 16:47:57 +0100

    updated example

--- src/example.py
+++ src/example.py
@@ -3,15 +3,18 @@ from Scweet.user import get_user_information, get_users_following, get_users_fol
 
 # scrape top tweets with the words 'covid','covid19' in proximity and without replies. ",
 # the process is slower as the interval is smaller (choose an interval that can divide the period of time betwee, start and max date) ",
-data = scrap(words=['covid','covid19'], 
+df = scrap(words=['covid','covid19'], 
     start_date="2020-04-01", 
     max_date="2020-04-15", 
     from_account = None,
     interval=1, 
     headless=True, 
     display_type="Top", 
-    save_images=False,
+    save_images=True,
     resume=False, 
     filter_replies=True, 
     proximity=True,
     save_dir="/root/twitter")
+
+# look dataframe head
+df.head()
